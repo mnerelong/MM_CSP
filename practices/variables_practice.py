4edfc485366e9522a6047f5e7cffb05f4cc047c1
mnerelong <meika.milton@ucas-edu.net>
2025-09-02 23:28:08 +0000

    Variables Practice

--- practices/variables_practice.py
+++ practices/variables_practice.py
@@ -13,7 +13,9 @@ numberB = 411
 print(numberA, "+", numberB, "=", numberA + numberB)
 
 favColor = input("Pick a color, any color!\n")
-print("I was going to say something nice about your favorite color but", favColor, "sucks. Your", eyeColor, "eyes are much prettier.")
+print("I was going to say something nice about your favorite color, but", favColor, "sucks. Your", eyeColor, "eyes are much prettier.")
 
 ageOfYou =  input("How old are you, hmm?\n")
 favClass = input("You got a favorite class?\n")
+
+print("You are", ageOfYou, "years old and participating in", favClass, "over something like... I don't know- drawing? Freak.")
