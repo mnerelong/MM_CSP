ff589b8e351c03dbd3c4e683497be98dc728addb
mnerelong <meika.milton@ucas-edu.net>
2025-09-02 13:44:24 -0600

    variables practice

--- practices/variables_practice.py
+++ practices/variables_practice.py
@@ -1,1 +1,19 @@
 # MM 6th Variables Practice
+
+school = "UCAS"
+year = 2411
+eyeColor = "black"
+user = input("What is your name?\n")
+print("Hello", user, "from", school + "!")
+breakfast = input("What did you eat today? Anything?\n")
+print(breakfast + "? That sounds disgusting.")
+
+numberA = 4.11
+numberB = 411
+print(numberA, "+", numberB, "=", numberA + numberB)
+
+favColor = input("Pick a color, any color!\n")
+print("I was going to say something nice about your favorite color but", favColor, "sucks. Your", eyeColor, "eyes are much prettier.")
+
+ageOfYou =  input("How old are you, hmm?\n")
+favClass = input("You got a favorite class?\n")
