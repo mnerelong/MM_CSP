c7a94cde201986b7de2e8d18964828ac2463d6db
mnerelong <meika.milton@ucas-edu.net>
2025-09-04 13:43:51 -0600

    started expressions notes

---
+++ notes/expressions.py
@@ -0,0 +1,45 @@
+# MM 6th Expressions Notes
+
+# What is an algorithm?
+    # An expression of numbers and symbols in order to get a result; a method and a process for creating an output.
+region = input("Where are you from?\n")
+print("Oh!", region + "? I hate that place! That's awful! You should be ashamed.")
+
+length = 4
+width = 11
+area = length * width
+print(length, "x", width, "=", area )
+
+# List steps in an algorithm.
+    #1 VARIABLES/INFORMATION
+person1 = 14
+person2 = 14
+person3 = 14
+person4 = 14
+
+    #2 PROCESS
+average = (person1 + person2 + person3 + person4)/4
+
+    #3 OUTPUT RESULT(S)
+print("The average age for my row in CSP is", average)
+
+#AGAIN!
+person1 = 41
+person2 = 24
+person3 = 104
+person5 = 504
+
+average = (person1 + person2 + person3 + person4)/4
+print("The average age in the placeholder group that does not exist is", average)
+
+# List  ALL of the different mathematical operators (give me the symbols and tell me what it does).
+num1 = 24
+num2 = 7
+
+print("addition:", num1 + num2)
+print("subtraction:", num1 - num2)
+print("multiplication:", num1 * num2)
+print("division:", num1 / num2)
+print("exponents:", num1 ** num2)
+print("integer division:", num1 // num2)
+print("modulo (remainder):", num1 % num2)
