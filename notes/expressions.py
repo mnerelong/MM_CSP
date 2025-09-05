791f6d8740f3e17867785d3f43ce86ae94c1949d
mnerelong <meika.milton@ucas-edu.net>
2025-09-05 13:45:02 -0600

    finished notes

--- notes/expressions.py
+++ notes/expressions.py
@@ -33,13 +33,35 @@ average = (person1 + person2 + person3 + person4)/4
 print("The average age in the placeholder group that does not exist is", average)
 
 # List  ALL of the different mathematical operators (give me the symbols and tell me what it does).
-num1 = 24
-num2 = 7
+num1 = float(input("Pick a number, any number!\n"))
+num2 = int(input("Pick another number! A better number!\n"))
 
-print("addition:", num1 + num2)
-print("subtraction:", num1 - num2)
-print("multiplication:", num1 * num2)
-print("division:", num1 / num2)
-print("exponents:", num1 ** num2)
-print("integer division:", num1 // num2)
-print("modulo (remainder):", num1 % num2)
+num1 += num2
+print("addition (+):", round(num1, 0)) #adding
+num1 -= num2
+print("subtraction (-):", round(num1, 0)) #subtracting
+num1 *= num2
+print("multiplication (*):", round(num1, 0)) #multiplying
+num1 /= num2
+print("division (/):", round(num1, 0)) #dividing
+num1 **= num2
+print("exponents (**):", round(num1, 0)) #exponents
+num1 //= num2
+print("integer division (//):", round(num1, 0)) #divides into whole numbers
+num1 %= num2
+print("modulo (remainder) (%):", round(num1, 0)) #remainder, the other side of integer division
+
+    #an integer is a data type that holds whole numbers. Can be positive or negative.
+    #a float (floating point) is a data type that holds numbers with decimals.
+
+# List ALL of the different assignment operators (give me the symbols and what they do.
+print(int(3*5**2/15)-(5-2**2))
+    #  int makes it an integer!
+print(float(3*5**2/15)-(5-2**2))
+    #  float makes it a float!
+num3 = 4.59
+print(int(round(num3, 0) ) )
+    # rounds to however many decimals you want AND makes it an integer.
+
+# Why are expressions so important in programming?
+    #Because it is a way for the computer to process code and solve math problems for us. We would  not be able to have health bars or currency trackers in video games/
---
+++ practices/expressions_practice.py
@@ -0,0 +1,28 @@
+# MM 6th Expressions Practice
+
+print("7 - 24 / 8 * 4 + 6 =", 7 - 24 / 8 * 4 + 6 )
+print("", )
+print("", )
+print("", )
+print("", )
+print("", )
+print("", )
+print("", )
+print("", )
+
+
+    #ignore all of this
+
+#print("", )
+#print("", )
+#print("", )
+#print("", )
+
+#income = 3000
+#rent = 1200
+#utilities = 200
+#transport = 500
+#savings = income * 0.1
+#float(round(eveyrting or something, 2))
+#percent_rent = 1200/3000
+#total =income + rent + utilities + transport + savings
