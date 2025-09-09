fe10113a3a30cc38386677aaa213d1b267be17fb
mnerelong <meika.milton@ucas-edu.net>
2025-09-09 13:44:29 -0600

    finished  strings notes

--- notes/string.py
+++ notes/string.py
@@ -15,8 +15,11 @@ string = "This is a string"
     # It is the only data type that can store letters.
 
 # QUESTION: How do stupid proofing and sanitizarion relate to eachother?
-    # .strip() gets rid of excess spaces, .upper() makes everything uppercase and .lower() makes everything lowercase, .capitalize() capitalizes
+    # .strip() gets rid of excess spaces, .upper() makes everything uppercase and .lower() makes everything lowercase, .capitalize() capitalizes the first letter of the word, .title() makes the first letterr of every word in a string uppercase, .find() finds the
+    #index number of a certain item and outputs it, concatenation is just adding two strings together.
     # 
+print(string.find("string"))
+print(string[10:16])
 name = input("Who are you? \n").strip().title()
 print(name + "? How ugly. Welcome.")
 
@@ -27,7 +30,7 @@ mother_name = mother_firname + " " + mother_secname
 print(mother_name + "? I think I've heard of her. Thanks, I'll be calling her now.")
 
 # QUESTION : What is debugging?
-    # Debugging is fixing errors (bugs) in your code.
-    # Syntax Error :
-    # Logic Error :
-    # Runtime Error :
+    # Debugging is fixing bugs in your code. A bug is any error in your code that keeps it from running or causes it to run improperly.
+    # Syntax Error : Any code that has been written incorrectly, like a spelling error or grammar/punctuation issues in english.
+    # Logic Error : When our code does something that was didn't expect it to do. It is a problem with the steps we took to complete our process, a problem with our "logic."
+    # Run-Time Error : Prevents the code from running and breaks it. Doesn't bring up a syntax. Like a bad user input or a mispelled variable. 
