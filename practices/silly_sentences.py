8ab1af1f916f6f43d48afed505261602ef1c1e10
mnerelong <meika.milton@ucas-edu.net>
2025-09-10 13:41:55 -0600

    wip silly sentences

--- practices/silly_sentences.py
+++ practices/silly_sentences.py
@@ -1,2 +1,13 @@
 # MM 6th Silly Sentences
 
+name1 = input("What's your name? \n").lower().title()
+name2 = input("Who is your best friend? Name..? \n").lower().title()
+verb1 = input("Pick a verb ending in -ing. \n").lower()
+location1 = input("Pick a location \n").lower()
+adjective1 = input("Pick an adjective. \n").lower()
+noun1 = input("Pick a noun. \n").lower()
+verb2 = input("Pick a verb ending in -ing. \n").lower()
+verb3 = input("Pick a verb in the past tense. \n").lower()
+adjective2 = input("Pick an adjective. \n").lower()
+
+print(f'{name1} and {name2} were {verb1} around in {location1} when all of the sudden a {adjective1} {noun1} came {verb2} at them. {name1} {verb3} out of the way but {name2} wasnt')
