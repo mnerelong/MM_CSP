afa1c69c1810034c79a48220c07f542adc191096
mnerelong <meika.milton@ucas-edu.net>
2025-09-16 13:45:42 -0600

    upodatre

---
+++ practices/update_helloworld.py
@@ -0,0 +1,16 @@
+# MM 6th Update Hello World
+
+def hi(name):
+    print(f"Hello {name}! Good to meet you.")
+
+def greet(name):
+    name = input("What might your name be?\n")
+    return name
+
+user = greet(input("What might your name be?"))
+hi(user)
+
+
+#ORIGINAL CODE
+    #first = input("What might your name be?\n")
+    #print("Hello", first + "!" )
