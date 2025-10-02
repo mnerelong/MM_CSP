cd329cb33c5002783344c9ab21cbca5962efa909
mnerelong <meika.milton@ucas-edu.net>
2025-10-02 13:44:24 -0600

    cvariables10022025

---
+++ c_programming/notes/variables.c
@@ -0,0 +1,14 @@
+// MM 6th CSP Variables Notes
+#include <stdio.h>
+
+int main(void){
+    int num = 411; // have to set what type of data the variable will hold. Static data types (cannot be changed).
+    float num_two = 4.11;
+    char one_letter = 'N'; // will only hold one letter.
+    char multiple[]= "Rain World"; // has to be a list. lists made by char name[] !!!<- BRACKETS!!! = "stuff"
+    
+    printf("%d", num);
+
+    return 0;
+}
+// python has dynamic variables.
