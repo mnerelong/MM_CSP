28c341298c9f5e774ef86fcfdf4bdf8b2ed152d0
mnerelong <meika.milton@ucas-edu.net>
2025-10-23 13:44:54 -0600

    hangman test 102325

--- c_programming/misc.c
+++ c_programming/misc.c
@@ -4,37 +4,8 @@
 #include <string.h>
 #include <time.h>
 
-char ask_guess(void){
-	char input;
-	printf("please pick a letter: "); 
-	scanf("%c", &input);
-	return input;
-}
-
 int main(void){
-    srand(time(NULL));
-    int random = rand() %5 +1;
-    char word[10];
-
-    if (random == 1){
-        char* word = "build"; 
-    } else if (random == 2) {
-        char* word = "lovely";
-    } else if (random == 3) {
-        char* word = "game";
-    } else if (random == 4) {
-        char* word = "first";
-    } else {
-        char* word = "five";
-    }
-    printf("%s", word);
-    char one = ask_guess();
-
-    if (strchr(word, one) != NULL){
-        printf("Correct! %c is in %s!\n", one, word);
-    } else {
-        printf("INCORRECT! %c is NOT in %s!\n", one, word);
-    }
-
+char hangman;
+printf(" o\n/|-- \n ||");
     return 0;
 }
