8984037dee94540c304cd22689fde80fccc5a654
mnerelong <meika.milton@ucas-edu.net>
2025-10-25 02:03:41 +0000

    dont lose it DO NOT LOSE YOUR CODE 10242025

--- c_programming/misc.c
+++ c_programming/misc.c
@@ -5,13 +5,24 @@
 #include <time.h>
 
 int main(void){
-
-printf("  o\n /|\\ \n /\\");
+/*printf("  o\n /|\\ \n /\\\n");*/
 srand(time(NULL));
 int wrong_attempt_tracker = rand() %6 +0;
+printf("%i\n", wrong_attempt_tracker);
 if (wrong_attempt_tracker == 0){
-    
-    
+    printf("\n\n\n");
+} else if (wrong_attempt_tracker == 1){
+    printf("  o\n\n\n");
+} else if (wrong_attempt_tracker == 2){
+    printf("  o\n  |\n\n");
+} else if (wrong_attempt_tracker == 3 ){
+    printf("  o\n /| \n\n");
+} else if (wrong_attempt_tracker == 4 ){
+    printf("  o\n /|\\ \n\n");
+} else if (wrong_attempt_tracker == 5 ){
+    printf("  o\n /|\\ \n /\n");
+} else {
+    printf("  o\n /|\\ \n /\\\n");
 }
     return 0;
 }
